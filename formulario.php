d738df3e726c04b33b9dd5d2b4919a63a23ef231
Fani-19 <estefbd@hotmail.com>
2020-07-10 16:32:40 +0200

    paso 1 creacion plugin

---
+++ formulario.php
@@ -0,0 +1,12 @@
+<?php
+/** 
+*Plugin Name: Formulario Autoevaluacion
+Plugin URI: https://autoevaluacion.com/
+Description: Formulario autoevaluacion
+Version: 1.0
+Author:Fani
+Author URI: https://fanidiseños.com/
+License: GPLv2 or later
+Text Domain: formulario
+*/
+?>
